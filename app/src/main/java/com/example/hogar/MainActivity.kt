da5d0c0940f08f908e50d3a3d6deeb72ec42685f
Juan Esteban Mazo Herrera - Ceiba Software <juan.mazo@MacBook-Pro-de-soporteceibalocal.local>
2020-12-09 14:02:56 -0500

    Initial commit

---
+++ app/src/main/java/com/example/hogar/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.hogar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        prueba()
+    }
+
+    fun prueba(){
+        var texto: TextView
+        texto = findViewById(R.id.hello)
+        texto.setText("Modificado")
+    }
+    
+}
